5470f7e8db0496e58aaa8e94613d9e1ad01060f4
Aaroah Ranjan <aaroah.ranjan@gmail.com>
2021-10-04 19:27:09 -0700

    some message

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+let [item , setItem] = useState({text : '', id :  null })
+let [itemList , setItemList] = useState([])
+
+let style ={
+height : '50px'
+}
+
+let type = (e)=>{
+setItem({text : e.target.value , id : Date.now() })
+}
+let addItem =(e)=>{
+  if(item.text === ''){
+  return
+}
+  setItemList([...itemList , item])
+  setItem({ text : '' , id : null})
+  e.target.previousSibling.focus()
+}
+let remove =(id) =>{
+  let newList = [];
+  for( let x = 0 ; x < itemList.length; x++){
+    if( id === itemList[x].id){
+      // ..
+    }
+  else{
+newList.push(itemList[x])
+  }
+}
+  setItemList(newList)
+}
+  return (
+    <div className="container">
+      <div className="hello"><input type="text" placeholder='Enter text' value={item.text} onChange={type} />
+      <button onClick={addItem } className='add-button'>Add item</button>
+      </div>
+  { 
+      
+        itemList.map(function(name) {
+          return(
+            <div className='item'>
+              <div className="itme-text">{name.text}</div>
+              <div className="checkbox"><input type="checkbox" /> </div>
+              <div className="remove-div"><button className='remove' onClick={() =>{remove(name.id)}}>remove</button></div>
+            </div>
+          ) 
+        })
+      }
+    
+    </div>
+  );
+}
+
+export { App}; 
